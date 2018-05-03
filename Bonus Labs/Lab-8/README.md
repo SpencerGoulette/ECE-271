ef2e26c8edc3b1bdaee4dc6da82bcdaace3b922d
Spencer Goulette <32938977+SpencerGoulette@users.noreply.github.com>
2018-05-03 12:51:39 -0700

    Uploaded final lab 8 files

--- Bonus Labs/Lab-8/README.md
+++ Bonus Labs/Lab-8/README.md
@@ -5,20 +5,24 @@ Spring 2018
 Name
 -----------
 ```
-WRITE YOUR NAME HERE
+Spencer Goulette
 ```
 
 Summary of Lab Status
 -------
-- [ ] Pre-lab Assignment (2 points) 
-- [ ] Basic in-lab assignments (15 points) 
-- [ ] Something cool (3 points): 
+- [X] Pre-lab Assignment (2 points) 
+- [X] Basic in-lab assignments (15 points) 
+- [X] Something cool (3 points): 
 
 What is your something cool?
 -------
+1. Using different joystick controls, different things happwn with the LEDs and the LCD.
 
 Post-lab Questions
 -------
+1. If the interrupt handler flags were not cleared, it does not work. It doesn't work because since the flag isn't cleared, it is constantly busy and keeps running through the interrupt.
+2. Software is unable to set the interrupt flag bit, but it can set a different bit that will make the hardware set the interrupt flag bit. So, in a way, software can set the flag by setting another bit which makes hardware set the flag.
+
 
 Lab Credits
 -------
--- Bonus Labs/Lab-8/main.c
+++ Bonus Labs/Lab-8/main.c
@@ -1,31 +1,34 @@
 #include "stm32l476xx.h"
-
+#include "lcd.h"
 /* Spencer Goulette
-	 03/01/18
-	 ECE 271 LAB6
-	 Objective: Get LED to blink with
-	 Pins: Port B pins used for red LED
-	 Clock Frequency: 8MHz
-	 SysTick Clock Frequency: 1MHz
+	 05/03/18
+	 ECE 271 Bonus Lab 8
+	 Objective: Get LED to blink with another LED using external interrupt
+	 Pins: Port B pins used for red LED and Port E pins used for green LED
+	 Clock Frequency: 16MHz
 */
 
 void clockInit(void);
 void portInit(void);
 void EXTI_Init(void);
+void EXTI9_5_IRQHandler(void);
 void EXTI3_IRQHandler(void);
+void EXTI2_IRQHandler(void);
+void EXTI1_IRQHandler(void);
+void EXTI0_IRQHandler(void);
+void delay(int time);
 
 int main(void)
 {
 	int i;
+	LCD_Initialization();	// Intitializing everything
 	clockInit();
-	
 	portInit();
-	
 	EXTI_Init();
 	
 	while(1)
 	{
-		GPIOB->ODR ^= GPIO_ODR_ODR_2;
+		GPIOB->ODR ^= GPIO_ODR_ODR_2;	// Toggles LED
 		for(i = 0; i < 1000000; i++)
 		{
 			
@@ -54,6 +57,7 @@ void clockInit(void)
   // Wait till HSI is used as system clock source 
   while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) == 0 ) {;}
 }
+
 void portInit(void)
 {
 	// Enable the clock to GPIO Port B	
@@ -96,32 +100,131 @@ void EXTI_Init(void)
 	// Enable SYSCFG clock
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 	
-	// Select PA.3 as the trigger source of EXTI 3
-	SYSCFG->EXTICR[0] &= ~SYSCFG_EXTICR1_EXTI3;
-	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI3_PA;
+	// Select the trigger sources of the EXTIs
+	SYSCFG->EXTICR[0] &= ~(SYSCFG_EXTICR1_EXTI3 | SYSCFG_EXTICR1_EXTI2 | SYSCFG_EXTICR1_EXTI1 | SYSCFG_EXTICR1_EXTI0);
+	SYSCFG->EXTICR[0] |= (SYSCFG_EXTICR1_EXTI3_PA | SYSCFG_EXTICR1_EXTI2_PA | SYSCFG_EXTICR1_EXTI1_PA | SYSCFG_EXTICR1_EXTI0_PA);
 	SYSCFG->EXTICR[0] &= ~(0x000F);
 	
-	// Enable rising edge trigger for EXTI3
+	SYSCFG->EXTICR[1] &= ~(SYSCFG_EXTICR2_EXTI5);
+	SYSCFG->EXTICR[1] |= (SYSCFG_EXTICR2_EXTI5_PA);
+	SYSCFG->EXTICR[1] &= ~(0x000F);
+	
+	// Enable rising edge trigger 
 	// Rising trigger selection register (RSTR)
 	// 0 = disabled, 1 = enabled
-	EXTI->RTSR1 |= EXTI_RTSR1_RT3;
+	EXTI->RTSR1 |= (EXTI_RTSR1_RT3 | EXTI_RTSR1_RT2 | EXTI_RTSR1_RT1 | EXTI_RTSR1_RT0);
 	
-	// Disable falling edge trigger for EXTI3
+	EXTI->RTSR1 |= EXTI_RTSR1_RT5;
+	
+	// Disable falling edge trigger
 	// Falling trigger selection register (FSTR)
 	// 0 = disabled, 1 = enabled
-	EXTI->FTSR1 &= ~EXTI_FTSR1_FT3;
+	EXTI->FTSR1 &= ~(EXTI_FTSR1_FT3 | EXTI_FTSR1_FT2 | EXTI_FTSR1_FT1 | EXTI_FTSR1_FT0);
 	
-	// Enable EXTI 3 interrupt
+	EXTI->FTSR1 &= ~EXTI_FTSR1_FT5;
+	
+	// Enable EXTI interrupts
 	// Interupt mask register: 0 = masked, 1 = unmasked
 	// "Masked" means that processor ignores the corresponding interrupt.
-	EXTI->IMR1 |= EXTI_IMR1_IM3;
+	EXTI->IMR1 |= (EXTI_IMR1_IM3 | EXTI_IMR1_IM2 | EXTI_IMR1_IM1 | EXTI_IMR1_IM0);
 	
-	// Enable EXTI 3 interrupt
+	EXTI->IMR1 |= (EXTI_IMR1_IM5);
+	
+	// Enable EXTI interrupts
+	NVIC_EnableIRQ(EXTI9_5_IRQn);
 	NVIC_EnableIRQ(EXTI3_IRQn);
+	NVIC_EnableIRQ(EXTI2_IRQn);
+	NVIC_EnableIRQ(EXTI1_IRQn);
+	NVIC_EnableIRQ(EXTI0_IRQn);
 	
-	// Set EXTI 3 priority to 1
+	// Set EXTI priority to 1
+	NVIC_SetPriority(EXTI9_5_IRQn, 1);
 	NVIC_SetPriority(EXTI3_IRQn, 1);
+	NVIC_SetPriority(EXTI2_IRQn, 1);
+	NVIC_SetPriority(EXTI1_IRQn, 1);
+	NVIC_SetPriority(EXTI0_IRQn, 1);
 	
+}
+
+void EXTI9_5_IRQHandler(void)
+{
+	if((EXTI->PR1 & EXTI_PR1_PIF5) == EXTI_PR1_PIF5)
+	{
+		
+		// Display LCD
+		GPIOB->ODR &= ~GPIO_ODR_ODR_2;	// Turn both off
+		GPIOE->ODR &= ~GPIO_ODR_ODR_8;
+		
+		// Clear interrupt pending request
+		EXTI->PR1 |= EXTI_PR1_PIF5;
+	}
+}
+
+void EXTI0_IRQHandler(void)
+{
+	if((EXTI->PR1 & EXTI_PR1_PIF0) == EXTI_PR1_PIF0)
+	{
+		
+		// Display LCD
+		GPIOB->ODR |= GPIO_ODR_ODR_2;	// Turn both on
+		GPIOE->ODR |= GPIO_ODR_ODR_8;
+		
+		// Clear interrupt pending request
+		EXTI->PR1 |= EXTI_PR1_PIF0;
+	}
+}
+
+void EXTI1_IRQHandler(void)
+{
+	// Check for EXTI 3 interrupt flag
+	if((EXTI->PR1 & EXTI_PR1_PIF1) == EXTI_PR1_PIF1)
+	{
+		
+		// Display LCD
+		delay(500);
+		LCD_DisplayString((uint8_t*)"BONUS");
+		delay(500);
+		LCD_DisplayString((uint8_t*)"ONUS ");
+		delay(500);
+		LCD_DisplayString((uint8_t*)"NUS L");
+		delay(500);
+		LCD_DisplayString((uint8_t*)"US LA");
+		delay(500);
+		LCD_DisplayString((uint8_t*)"S LAB");
+		delay(500);
+		
+		// Clear LCD
+		LCD_Clear();
+		
+		// Clear interrupt pending request
+		EXTI->PR1 |= EXTI_PR1_PIF1;
+	}
+}
+
+void EXTI2_IRQHandler(void)
+{
+	static int toggle = 1;
+	// Check for EXTI 3 interrupt flag
+	if((EXTI->PR1 & EXTI_PR1_PIF2) == EXTI_PR1_PIF2)
+	{
+		if(toggle == 1)
+		{
+			// Display LCD
+			LCD_DisplayString((uint8_t*)"BONUS");
+			
+			// Toggle
+			toggle = 0;
+		}
+		else{
+			// Clear LCD
+			LCD_Clear();
+			
+			// Toggle
+			toggle = 1;
+		}
+		// Clear interrupt pending request
+		EXTI->PR1 |= EXTI_PR1_PIF2;
+	}
 }
 
 void EXTI3_IRQHandler(void)
@@ -135,5 +238,11 @@ void EXTI3_IRQHandler(void)
 		// Clear interrupt pending request
 		EXTI->PR1 |= EXTI_PR1_PIF3;
 	}
+}
+
+void delay(int time)	// Delay function
+{
+	volatile int i;
+	for(i = 0; i < time * 1000; i++);
 }
 
